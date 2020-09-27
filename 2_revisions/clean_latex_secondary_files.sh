1cb7e74eb33e55cb5be3ee6cb8e0a3b325dd0331
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2020-09-28 04:10:44 +0900

    First data commit: V2-1, before second review

---
+++ 2_revisions/clean_latex_secondary_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+find . -name '*.blg' -type f -delete
+find . -name '*.bbl' -type f -delete
+find . -name '*.aux' -type f -delete
+find . -name '*.log' -type f -delete
+find . -name '*.thm' -type f -delete
+find . -name '*.out' -type f -delete
+find . -name '*.spl' -type f -delete
+find . -name '*.gz' -type f -delete
