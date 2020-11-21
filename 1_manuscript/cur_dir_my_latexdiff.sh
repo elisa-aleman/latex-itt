c406a30119154830106d0026950cd51d4874e94c
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2020-11-22 00:31:49 +0900

    Added latexdiff for comments from teacher

--- 1_manuscript/cur_dir_my_latexdiff.sh
+++ 1_manuscript/cur_dir_my_latexdiff.sh
@@ -1,1 +1,1 @@
-../my_latexdiff.sh itt-emotion_V2-1.tex itt-emotion.tex --newversion="3" --compile --view --clean
+../my_latexdiff.sh itt-emotion_V3.tex itt-emotion.tex --newversion="4" --compile --view --clean
