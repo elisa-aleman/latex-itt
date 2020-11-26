75bfa0185a35abb174107c7d73d03d0680613424
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2020-11-26 19:10:03 +0900

    Added to revisions folder

---
+++ 2_revisions/V4_ITT_minor_revision/0_manuscript_blind/cur_dir_my_latexdiff.sh
@@ -0,0 +1,1 @@
+../my_latexdiff.sh itt-emotion_blind_V3.tex itt-emotion_blind.tex -d --newversion="4" --compile --view --clean
---
+++ 2_revisions/V4_ITT_minor_revision/1_manuscript/cur_dir_my_latexdiff.sh
@@ -0,0 +1,1 @@
+../my_latexdiff.sh itt-emotion_V3.tex itt-emotion.tex --newversion="4" --compile --view --clean 
