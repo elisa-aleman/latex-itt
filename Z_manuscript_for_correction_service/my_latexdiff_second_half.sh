574cd31cc7b9dc383a076429f07794958afbfa98
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2021-02-16 02:56:01 +0900

    Added 2nd half corrections after checking, have to checkout to look for V5 since it got deleted

---
+++ Z_manuscript_for_correction_service/my_latexdiff_second_half.sh
@@ -0,0 +1,3 @@
+# ../my_latexdiff.sh itt-emotion_second_half_V1.tex itt-emotion_second_half_V2_service.tex --newversion="V2_service" --compile --view --clean 
+
+../my_latexdiff.sh itt-emotion_second_half_V1.tex itt-emotion_second_half_V3_service_corrected.tex --newversion="V3_service_corrected" --compile --view --clean 
