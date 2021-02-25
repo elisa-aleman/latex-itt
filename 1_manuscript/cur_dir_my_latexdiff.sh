dcac91301c6a99f1ec210dd36d8916eb2def92d6
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2021-02-25 19:25:29 +0900

    Made V6-2, -1800 words

--- 1_manuscript/cur_dir_my_latexdiff.sh
+++ 1_manuscript/cur_dir_my_latexdiff.sh
@@ -1,1 +1,8 @@
-../my_latexdiff.sh itt-emotion_V5.tex itt-emotion.tex --newversion="6" --compile --view --clean 
+# ../my_latexdiff.sh itt-emotion_V5.tex itt-emotion.tex --newversion="6" --compile --view --clean 
+
+# ../my_latexdiff.sh itt-emotion_V5.tex itt-emotion_V6-1.tex --compile --view --clean
+# ../my_latexdiff.sh itt-emotion_V6-0.tex itt-emotion_V6-1.tex --compile --view --clean
+
+../my_latexdiff.sh itt-emotion_V5.tex itt-emotion_V6-2.tex --compile --clean
+../my_latexdiff.sh itt-emotion_V6-0.tex itt-emotion_V6-2.tex --compile --clean
+../my_latexdiff.sh itt-emotion_V6-1.tex itt-emotion_V6-2.tex --compile --clean
