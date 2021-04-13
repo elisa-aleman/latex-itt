5e0c6c89e41db71d6d739908f7a81374750c5962
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2021-04-14 04:40:53 +0900

    V7-2 after showing to Nonaka again, preparing all files, cover letter still to be written yet

--- 1_manuscript/cur_dir_my_latexdiff.sh
+++ 1_manuscript/cur_dir_my_latexdiff.sh
@@ -1,1 +1,1 @@
-../my_latexdiff.sh itt-emotion_V6-6.tex itt-emotion.tex --newversion="7-1" --compile --clean --view
+../my_latexdiff.sh itt-emotion_V6-6.tex itt-emotion.tex --newversion="7-2" --compile --clean --view
